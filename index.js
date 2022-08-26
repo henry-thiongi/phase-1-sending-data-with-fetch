144ac6e87d81075bdf081ed982e4594330126453
henry-thiongi <boblilhot@gmail.com>
2022-08-26 06:14:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name,email){
+    const options = 
+{
+    method : "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        name,
+        email
+    })
+} 
+
+   return fetch('http://localhost:3000/users',options)
+   .then(resp=>resp.json())
+   .then(data=>document.body.innerHTML =data['id'] )
+   .catch(error=>document.body.innerHTML = error.message)
+
+} 
